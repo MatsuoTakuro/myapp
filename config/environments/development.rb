95fa6f27b3f795574de56bd63f1725472224ab80
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-07-10 08:50:25 +0900

    Create; allow to connections to local server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # リスト 1.7: ローカルWebサーバーへの接続を許可する
+  config.hosts.clear
 end
