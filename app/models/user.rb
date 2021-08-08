33238127bbafb91c3fed78f8eefcb1c7d2011b8d
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-08-09 00:02:51 +0900

    feat: update to リスト 9.13: green

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    # before_save { self.email = email.downcase }
-    before_save { email.downcase! }
+    attr_accessor :remember_token
+    before_save { self.email = email.downcase }
+
 
     validates :name,  presence: true, length: { maximum: 50 }
 
@@ -21,5 +22,20 @@ class User < ApplicationRecord
     # ランダムなトークンを返す
     def User.new_token
         SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        self.update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticated?(remember_token)
+        BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
+    end
+
+    # ユーザーのログイン情報を破棄する
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 end
