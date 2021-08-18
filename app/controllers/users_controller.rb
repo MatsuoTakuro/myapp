30215904d63f85abbd428f5a4930358d8ab3b9fc
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-08-18 14:02:54 +0900

    feat: update to 第11章 アカウントの有効化, though 11.4 本番環境でのメール送信 is skipped.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,13 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   # GET /users/1
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   # GET /users/new
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,6 +8,17 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   end
 
   test "index as admin including pagination and delete links" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {  name:  "Example User",
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } }
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
@@ -15,6 +26,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      assert user.activated?
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
